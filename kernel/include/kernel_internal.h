d4c152228389b61aa1e92aac00055ac0ee9f1e71
ndrs-pst <50133174+ndrs-pst@users.noreply.github.com>
2022-05-02 15:38:19 +0700

    add: bindings/sbc/nxp,mc33903.yaml

--- kernel/include/kernel_internal.h
+++ kernel/include/kernel_internal.h
@@ -228,8 +228,8 @@ void z_mem_manage_boot_finish(void);
 /* #CUSTOM@NDRS, add 0 initialization in bracket { } */
 #define LOCKED(lck) for (k_spinlock_key_t __i = {0},  \
 					  __key = k_spin_lock(lck); \
-			!__i.key;					\
-			k_spin_unlock(lck, __key), __i.key = 1)
+					  !__i.key;					\
+					  k_spin_unlock(lck, __key), __i.key = 1)
 
 #ifdef CONFIG_PM
 
