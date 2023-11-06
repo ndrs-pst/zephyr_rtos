26429ba8cc3c217ef8222986b70bf43209f0bc0e
ndrs-pst <50133174+ndrs-pst@users.noreply.github.com>
2023-11-06 15:58:46 +0700

    soc: arm: atmel_sam0: fix z_arm_platform_init when CONFIG_SOC_ATMEL_SAMC_XOSC_AS_MAIN
    Fix improper setting of CFDPRESC when CFDEN = 1, which lead to failure in clock initialization.
    Fix Kconfig.samc2x value of CONFIG_SOC_ATMEL_SAMC_OSC48M_AS_MAIN

--- soc/arm/atmel_sam0/common/soc_samc2x.c
+++ soc/arm/atmel_sam0/common/soc_samc2x.c
@@ -25,62 +25,77 @@
 #error "Expect system to run at 48 MHz or 64 MHz !!!"
 #endif
 
+/* Adjust GAIN[2:0] value according to Recommended Max Frequency [MHz] of XOSC */
+#if (SOC_ATMEL_SAMC_XOSC_FREQ_HZ == 8000000UL)
+#define OSCCTRL_XOSCCTRL_GAIN_VAL           OSCCTRL_XOSCCTRL_GAIN(2UL)
+#elif (SOC_ATMEL_SAMC_XOSC_FREQ_HZ == 16000000UL)
+#define OSCCTRL_XOSCCTRL_GAIN_VAL           OSCCTRL_XOSCCTRL_GAIN(3UL)
+#else
+#error "Expect XOSC frequency at 8 MHz or 16 MHz !!!"
+#endif
+
+/*
+ *  XOSC32K(32.768 kHz)                     -> FDPLL96M -> GCLK0 (48 MHz)
+ *  OSC48M(OUT 16 MHz) / 16 = GCLK3(1 MHz)  -> FDPLL96M -> GCLK0 (48 MHz)
+ *  XOSC( 8 MHz) /  8        = 1 MHz        -> FDPLL96M -> GCLK0 (48 MHz)
+ *  XOSC(16 MHz) / 16        = 1 MHz        -> FDPLL96M -> GCLK0 (48 MHz)
+ */
+#define DPLLRATIO_LDR_VAL   \
+    (((SOC_ATMEL_SAM0_MCK_FREQ_HZ + (SOC_ATMEL_SAM0_FDPLL_IN_FREQ_HZ / 2)) / SOC_ATMEL_SAM0_FDPLL_IN_FREQ_HZ) - 1U)
+
+/**
+ * @brief Initialize NVM read wait state
+ * @see Table 45-41. NVM Max Speed Characteristics when operating upto +85 °C
+ */
 static void flash_waitstates_init(void) {
     if (SOC_ATMEL_SAM0_MCK_FREQ_HZ == 48000000UL) {
         /* One wait state at 48 MHz. */
-        NVMCTRL->CTRLB.bit.RWS = NVMCTRL_CTRLB_RWS_HALF_Val;
+        NVMCTRL->CTRLB.bit.RWS = 1U;
     }
 
     if (SOC_ATMEL_SAM0_MCK_FREQ_HZ == 64000000UL) {
         /* Two wait state at 64 MHz. */
-        NVMCTRL->CTRLB.bit.RWS = NVMCTRL_CTRLB_RWS_DUAL_Val;
+        NVMCTRL->CTRLB.bit.RWS = 2U;
     }
 }
 
-/*
- *  XOSC32K(32.768 kHz)                     -> DFLL48M -> GCLK0 (48 MHz)
- *  XOSC48M(OUT 16 MHz) / 16 = GCLK3(1 MHz) -> DFLL48M -> GCLK0 (48 MHz)
- *  XOSC( 8 MHz) /  8        = 1 MHz        -> DFLL48M -> GCLK0 (48 MHz)
- *  XOSC(16 MHz) / 16        = 1 MHz        -> DFLL48M -> GCLK0 (48 MHz)
+/**
+ * @brief Initialize 48MHz RC Oscillator (OSC48M)
+ * @note
+ * CFDPRESC: Since we're using CFD feature, care should be taken to set prescaler value large enough
+ * so that CFD clock is less than XOSC clock. With the 2^4 = (16) prescaler,
+ * CFD clock is 1 MHz (OSC48M which smaller than XOSC clock of 8 and 16 MHz.
  */
+static void osc48m_init(void) {
+    uint32_t cal_val;
 
-static void xosc_init(void) {
-    #if defined(CONFIG_SOC_ATMEL_SAMC_XOSC)
-    /* Configure External Oscillator */
-    OSCCTRL->XOSCCTRL.reg = (uint16_t)(OSCCTRL_XOSCCTRL_STARTUP(3UL) | OSCCTRL_XOSCCTRL_GAIN(2UL) |
-                                       OSCCTRL_XOSCCTRL_RUNSTDBY     | OSCCTRL_XOSCCTRL_CFDEN     |
-                                       OSCCTRL_XOSCCTRL_XTALEN       | OSCCTRL_XOSCCTRL_ENABLE);
-    while (OSCCTRL->STATUS.bit.XOSCRDY == 0) {
-        /* Waiting for the XOSC Ready state */
+    /****************** XOSC Initialization ********************************/
+    /* 20.8.6 Clock Failure Detector Prescaler */
+    /* The OSC48M oscillator is used to clock the CFD prescaler.
+       The CFD safe clock frequency is the OSC48M frequency divided by 2^CFDPRESC. */
+    OSCCTRL->CFDPRESC.reg = OSCCTRL_CFDPRESC_CFDPRESC(4U);      /* @warning shall set CFDPRESC before set CFDEN */
+
+    cal_val = (uint32_t)(((*(uint64_t*)0x806020UL) >> 41) & 0x3FFFFFUL);
+    OSCCTRL->CAL48M.reg = cal_val;
+
+    /* Selection of the Division Value (48 MHz / 3 = 16 MHz) */
+    OSCCTRL->OSC48MDIV.reg = (uint8_t)OSCCTRL_OSC48MDIV_DIV(2UL);
+
+    while (OSCCTRL->OSC48MSYNCBUSY.bit.OSC48MDIV == 1) {
+        /* Waiting for the synchronization */
     }
 
-    /* Setting the Automatic Gain Control */
-    OSCCTRL->XOSCCTRL.reg |= (uint16_t)OSCCTRL_XOSCCTRL_AMPGC;
-    #endif
-}
-
-static void wait_gclk_sync(uint32_t sync_bit) {
-    while ((GCLK->SYNCBUSY.reg & sync_bit) == sync_bit) {
-        /* pass */
+    while (OSCCTRL->STATUS.bit.OSC48MRDY == 0) {
+        /* Waiting for the OSC48M Ready state */
     }
+
+    OSCCTRL->OSC48MCTRL.bit.ONDEMAND = 0;
 }
 
-static void xosc32k_init(void) {
-    #ifdef CONFIG_SOC_ATMEL_SAMC_XOSC32K
-    OSC32KCTRL->XOSC32K.reg = (OSC32KCTRL_XOSC32K_STARTUP(1UL) | OSC32KCTRL_XOSC32K_ENABLE |
-                               OSC32KCTRL_XOSC32K_RUNSTDBY     | OSC32KCTRL_XOSC32K_EN32K  |
-                               OSC32KCTRL_XOSC32K_EN1K         | OSC32KCTRL_XOSC32K_XTALEN);
-
-    /* Enable clock failure detection */
-    OSC32KCTRL->CFDCTRL.bit.CFDEN = 1U;
-
-    /* Wait for the crystal to stabilize. */
-    while (!OSC32KCTRL->STATUS.bit.XOSC32KRDY) {
-        /* pass */
-    }
-    #endif
-}
-
+/**
+ * @brief Initialize 32.768 kHz Internal Oscillator (OSC32K)
+ * @see 47.6.3 32.768 kHz Internal Oscillator (OSC32K) Characteristics
+ */
 static void osc32k_init(void) {
     uint32_t cal_val;
 
@@ -95,7 +110,49 @@ static void osc32k_init(void) {
     }
 }
 
-#define DPLLRATIO_LDR_VAL   (((SOC_ATMEL_SAM0_MCK_FREQ_HZ + (SOC_ATMEL_SAM0_FDPLL_IN_FREQ_HZ / 2)) / SOC_ATMEL_SAM0_FDPLL_IN_FREQ_HZ) - 1U)
+/**
+ * @brief Initialize External Multipurpose Crystal Oscillator (XOSC) Control
+ * @note 
+ * CFDPRESC: Since we're using CFD feature, care should be taken to set prescaler value large enough
+ * so that CFD clock is less than XOSC clock. With the 2^4 = (16) prescaler,
+ * CFD clock is 1 MHz (OSC48M which smaller than XOSC clock of 8 and 16 MHz.
+ * 
+ * STARTUP[3:0]: @see Table 20-5. Start-Up Time for External Multipurpose Crystal Oscillator
+ * With the current setting value of 6, the start-up time is 1,953 μs.
+ */
+static void xosc_init(void) {
+    #if defined(CONFIG_SOC_ATMEL_SAMC_XOSC)
+    /* Configure External Oscillator */
+    OSCCTRL->XOSCCTRL.reg = (uint16_t)(OSCCTRL_XOSCCTRL_STARTUP(6UL) | OSCCTRL_XOSCCTRL_GAIN_VAL |
+                                       OSCCTRL_XOSCCTRL_RUNSTDBY     | OSCCTRL_XOSCCTRL_CFDEN    |
+                                       OSCCTRL_XOSCCTRL_XTALEN       | OSCCTRL_XOSCCTRL_ENABLE);
+    while (OSCCTRL->STATUS.bit.XOSCRDY == 0) {
+        /* Waiting for the XOSC Ready state */
+    }
+
+    /* Setting the Automatic Gain Control */
+    OSCCTRL->XOSCCTRL.bit.AMPGC = 1U;
+    #endif
+}
+
+static void xosc32k_init(void) {
+    #if defined(CONFIG_SOC_ATMEL_SAMC_XOSC32K)
+    OSC32KCTRL->XOSC32K.reg = (OSC32KCTRL_XOSC32K_STARTUP(1UL) | OSC32KCTRL_XOSC32K_ENABLE |
+                               OSC32KCTRL_XOSC32K_RUNSTDBY     | OSC32KCTRL_XOSC32K_EN32K  |
+                               OSC32KCTRL_XOSC32K_EN1K         | OSC32KCTRL_XOSC32K_XTALEN);
+
+    /* The CFD safe clock frequency is the OSCULP32K frequency divided by 2 */
+    OSC32KCTRL->CFDCTRL.bit.CFDPRESC = 1U;
+
+    /* Enable clock failure detection */
+    OSC32KCTRL->CFDCTRL.bit.CFDEN = 1U;
+
+    /* Wait for the crystal to stabilize. */
+    while (OSC32KCTRL->STATUS.bit.XOSC32KRDY == 0U) {
+        /* pass */
+    }
+    #endif
+}
 
 /// @note Errata 1.25.1 FDPLL Unlock
 /// When using FDPLL at temperature below 25°C, spurious DPLL unlocks (OSCCTRL.DPLLSTATUS.LOCK = 0)
@@ -105,7 +162,7 @@ static void osc32k_init(void) {
 /// When using FDPLL at temperature below 25°C, enable the lock bypass (OSCCTRL.DPLLCTRLB.LBYPASS = 1)
 /// to avoid losing FDPLL clock output during a false unlock status.
 static void fdpll_init(void) {
-    /****************** DPLL Initialization  *********************************/
+    /****************** DPLL Initialization *********************************/
     #if defined(CONFIG_SOC_ATMEL_SAMC_XOSC32K_AS_MAIN)
     /* Configure DPLL */
     if (SOC_ATMEL_SAM0_MCK_FREQ_HZ == 48000000UL) {
@@ -195,28 +252,14 @@ static void fdpll_init(void) {
     }
 }
 
-static void osc48m_init(void) {
-    uint32_t cal_val;
+static void mclk_init(void) {
+    MCLK->CPUDIV.reg = MCLK_CPUDIV_CPUDIV_DIV1_Val;
+}
 
-    /****************** XOSC Initialization   ********************************/
-    /* Selection of the Clock failure detection (CFD) pre scalar */
-    OSCCTRL->CFDPRESC.bit.CFDPRESC = OSCCTRL_CFDPRESC_CFDPRESC(0);
-
-    cal_val = (uint32_t)(((*(uint64_t*)0x806020UL) >> 41) & 0x3fffffUL);
-    OSCCTRL->CAL48M.reg = cal_val;
-
-    /* Selection of the Division Value (48 MHz / 3 = 16 MHz) */
-    OSCCTRL->OSC48MDIV.reg = (uint8_t)OSCCTRL_OSC48MDIV_DIV(2UL);
-
-    while (OSCCTRL->OSC48MSYNCBUSY.bit.OSC48MDIV == 1) {
-        /* Waiting for the synchronization */
+static void wait_gclk_sync(uint32_t sync_bit) {
+    while ((GCLK->SYNCBUSY.reg & sync_bit) == sync_bit) {
+        /* pass */
     }
-
-    while (OSCCTRL->STATUS.bit.OSC48MRDY == 0) {
-        /* Waiting for the OSC48M Ready state */
-    }
-
-    OSCCTRL->OSC48MCTRL.bit.ONDEMAND = 0;
 }
 
 static void gclks_init(void) {
@@ -254,6 +297,7 @@ void z_arm_platform_init(void) {
     xosc_init();
     xosc32k_init();
     fdpll_init();
+    mclk_init();
     gclks_init();
 
     /* Configure the AHB Bridge Clocks */
