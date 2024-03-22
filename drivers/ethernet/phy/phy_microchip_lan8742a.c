6d1bc77dd6b7e916a339fd41179cbddf5577b3b0
ndrs-pst <50133174+ndrs-pst@users.noreply.github.com>
2024-03-23 02:09:30 +0700

    drivers: ethernet: phy: add lan8742a physical driver (in-progress)

--- drivers/ethernet/phy/phy_microchip_lan8742a.c
+++ drivers/ethernet/phy/phy_microchip_lan8742a.c
@@ -66,11 +66,11 @@ static int phy_link_state_cmp(struct phy_link_state const* a_state, struct phy_l
 
 static int phy_mc_lan8742a_read(const struct device* dev,
                                 uint16_t reg_addr, uint32_t* data) {
-    const struct mc_lan8742a_config* config = dev->config;
+    const struct mc_lan8742a_config* cfg = dev->config;
     uint16_t tmp16;
     int ret;
 
-    ret = mdio_read(config->mdio_dev, config->addr, (uint8_t)reg_addr, &tmp16);
+    ret = mdio_read(cfg->mdio_dev, cfg->addr, (uint8_t)reg_addr, &tmp16);
     if (ret == 0) {
         *data = tmp16;
     }
@@ -80,71 +80,71 @@ static int phy_mc_lan8742a_read(const struct device* dev,
 
 static int phy_mc_lan8742a_write(const struct device* dev,
                                  uint16_t reg_addr, uint32_t data) {
-    const struct mc_lan8742a_config* config = dev->config;
+    const struct mc_lan8742a_config* cfg = dev->config;
     int ret;
 
-    ret = mdio_write(config->mdio_dev, config->addr, (uint8_t)reg_addr, (uint16_t)data);
+    ret = mdio_write(cfg->mdio_dev, cfg->addr, (uint8_t)reg_addr, (uint16_t)data);
 
     return (ret);
 }
 
 static int phy_mc_lan8742a_autonegotiate(const struct device* dev) {
-    struct mc_lan8742a_config const* config = dev->config;
+    struct mc_lan8742a_config const* cfg = dev->config;
     int ret;
-    uint32_t bmcr = 0;
-    uint32_t bmsr = 0;
+    uint32_t bmcr;
+    uint32_t bmsr;
     uint16_t timeout = CONFIG_PHY_AUTONEG_TIMEOUT_MS / 100;
 
     /* Read control register to write back with autonegotiation bit */
     ret = phy_mc_lan8742a_read(dev, MII_BMCR, &bmcr);
-    if (ret) {
-        LOG_ERR("Error reading phy (%d) basic control register", config->addr);
+    if (ret != 0) {
+        LOG_ERR("Error reading phy (%d) basic control register", cfg->addr);
         return (ret);
     }
 
     /* (re)start autonegotiation */
-    LOG_DBG("PHY (%d) is entering autonegotiation sequence", config->addr);
+    LOG_DBG("PHY (%d) is entering autonegotiation sequence", cfg->addr);
     bmcr |= MII_BMCR_AUTONEG_ENABLE | MII_BMCR_AUTONEG_RESTART;
     bmcr &= ~MII_BMCR_ISOLATE;
 
     ret = phy_mc_lan8742a_write(dev, MII_BMCR, bmcr);
-    if (ret) {
-        LOG_ERR("Error writing phy (%d) basic control register", config->addr);
+    if (ret != 0) {
+        LOG_ERR("Error writing phy (%d) basic control register", cfg->addr);
         return (ret);
     }
 
     /* TODO change this to GPIO interrupt driven */
     do {
         if (timeout-- == 0) {
-            LOG_DBG("PHY (%d) autonegotiation timed out", config->addr);
-            return -ETIMEDOUT;
+            LOG_DBG("PHY (%d) autonegotiation timed out", cfg->addr);
+            return (-ETIMEDOUT);
         }
         k_msleep(100);
 
         ret = phy_mc_lan8742a_read(dev, MII_BMSR, &bmsr);
-        if (ret) {
-            LOG_ERR("Error reading phy (%d) basic status register", config->addr);
+        if (ret != 0) {
+            LOG_ERR("Error reading phy (%d) basic status register", cfg->addr);
             return (ret);
         }
     } while (!(bmsr & MII_BMSR_AUTONEG_COMPLETE));
 
-    LOG_DBG("PHY (%d) autonegotiation completed", config->addr);
+    LOG_DBG("PHY (%d) autonegotiation completed", cfg->addr);
 
     return (0);
 }
 
 static int phy_mc_lan8742a_get_link(const struct device* dev,
                                     struct phy_link_state* state) {
-    struct mc_lan8742a_config const* config = dev->config;
-    struct mc_lan8742a_data* data = dev->data;
+    struct mc_lan8742a_config const* cfg = dev->config;
+    struct mc_lan8742a_data* ctx = dev->data;
     int ret;
     uint32_t bmsr;
     uint32_t anar;
     uint32_t anlpar;
-    struct phy_link_state old_state = data->state;
+    struct phy_link_state old_state = ctx->state;
 
     /* Lock mutex */
-    ret = k_mutex_lock(&data->mutex, K_FOREVER);
+    ret = k_mutex_lock(&ctx->mutex, K_FOREVER);
     if (ret != 0) {
         LOG_ERR("PHY mutex lock error");
         return (ret);
@@ -153,8 +153,8 @@ static int phy_mc_lan8742a_get_link(const struct device* dev,
     /* Read link state */
     ret = phy_mc_lan8742a_read(dev, MII_BMSR, &bmsr);
     if (ret != 0) {
-        LOG_ERR("Error reading phy (%d) basic status register", config->addr);
-        k_mutex_unlock(&data->mutex);
+        LOG_ERR("Error reading phy (%d) basic status register", cfg->addr);
+        k_mutex_unlock(&ctx->mutex);
         return (ret);
     }
     state->is_up = (bmsr & MII_BMSR_LINK_STATUS) ? true : false;
@@ -166,22 +166,22 @@ static int phy_mc_lan8742a_get_link(const struct device* dev,
 
     /* Read currently configured advertising options */
     ret = phy_mc_lan8742a_read(dev, MII_ANAR, &anar);
-    if (ret) {
-        LOG_ERR("Error reading phy (%d) advertising register", config->addr);
-        k_mutex_unlock(&data->mutex);
+    if (ret != 0) {
+        LOG_ERR("Error reading phy (%d) advertising register", cfg->addr);
+        k_mutex_unlock(&ctx->mutex);
         return (ret);
     }
 
     /* Read link partner capability */
     ret = phy_mc_lan8742a_read(dev, MII_ANLPAR, &anlpar);
-    if (ret) {
-        LOG_ERR("Error reading phy (%d) link partner register", config->addr);
-        k_mutex_unlock(&data->mutex);
+    if (ret != 0) {
+        LOG_ERR("Error reading phy (%d) link partner register", cfg->addr);
+        k_mutex_unlock(&ctx->mutex);
         return (ret);
     }
 
     /* Unlock mutex */
-    k_mutex_unlock(&data->mutex);
+    k_mutex_unlock(&ctx->mutex);
 
     uint32_t mutual_capabilities = (anar & anlpar);
 
@@ -203,8 +203,8 @@ static int phy_mc_lan8742a_get_link(const struct device* dev,
 
 result :
     if (phy_link_state_cmp(&old_state, state) != 0) {
-        LOG_DBG("PHY %d is %s", config->addr, state->is_up ? "up" : "down");
-        LOG_DBG("PHY (%d) Link speed %s Mb, %s duplex\n", config->addr,
+        LOG_DBG("PHY %d is %s", cfg->addr, state->is_up ? "up" : "down");
+        LOG_DBG("PHY (%d) Link speed %s Mb, %s duplex\n", cfg->addr,
                 (PHY_LINK_IS_SPEED_100M(state->speed) ? "100" : "10"),
                 PHY_LINK_IS_FULL_DUPLEX(state->speed) ? "full" : "half");
     }
@@ -234,7 +234,7 @@ static int phy_mc_lan8742a_static_cfg(const struct device* dev) {
             ~PHY_MC_LAN8742A_OMSO_NAND_TREE_MASK;
 
     ret = phy_mc_lan8742a_write(dev, PHY_MC_LAN8742A_OMSO_REG, omso);
-    if (ret) {
+    if (ret != 0) {
         return (ret);
     }
 
@@ -243,31 +243,31 @@ static int phy_mc_lan8742a_static_cfg(const struct device* dev) {
 
 static int phy_mc_lan8742a_cfg_link(const struct device* dev,
                                     enum phy_link_speed speeds) {
-    struct mc_lan8742a_config const* config = dev->config;
-    struct mc_lan8742a_data* data = dev->data;
+    struct mc_lan8742a_config const* cfg = dev->config;
+    struct mc_lan8742a_data* ctx = dev->data;
     int ret;
     uint32_t anar;
 
     /* Lock mutex */
-    ret = k_mutex_lock(&data->mutex, K_FOREVER);
-    if (ret) {
+    ret = k_mutex_lock(&ctx->mutex, K_FOREVER);
+    if (ret != 0) {
         LOG_ERR("PHY mutex lock error");
         goto done;
     }
 
     /* We are going to reconfigure the phy, don't need to monitor until done */
-    k_work_cancel_delayable(&data->phy_monitor_work);
+    k_work_cancel_delayable(&ctx->phy_monitor_work);
 
     /* DT configurations */
     ret = phy_mc_lan8742a_static_cfg(dev);
-    if (ret) {
+    if (ret != 0) {
         goto done;
     }
 
     /* Read ANAR register to write back */
     ret = phy_mc_lan8742a_read(dev, MII_ANAR, &anar);
-    if (ret) {
-        LOG_ERR("Error reading phy (%d) advertising register", config->addr);
+    if (ret != 0) {
+        LOG_ERR("Error reading phy (%d) advertising register", cfg->addr);
         goto done;
     }
 
@@ -302,33 +302,33 @@ static int phy_mc_lan8742a_cfg_link(const struct device* dev,
 
     /* Write capabilities to advertising register */
     ret = phy_mc_lan8742a_write(dev, MII_ANAR, anar);
-    if (ret) {
-        LOG_ERR("Error writing phy (%d) advertising register", config->addr);
+    if (ret != 0) {
+        LOG_ERR("Error writing phy (%d) advertising register", cfg->addr);
         goto done;
     }
 
     /* (re)do autonegotiation */
     ret = phy_mc_lan8742a_autonegotiate(dev);
-    if (ret) {
+    if (ret != 0) {
         LOG_ERR("Error in autonegotiation");
         goto done;
     }
 
     /* Get link status */
-    ret = phy_mc_lan8742a_get_link(dev, &data->state);
+    ret = phy_mc_lan8742a_get_link(dev, &ctx->state);
 
     /* Log the results of the configuration */
-    LOG_INF("PHY %d is %s", config->addr, data->state.is_up ? "up" : "down");
-    LOG_INF("PHY (%d) Link speed %s Mb, %s duplex\n", config->addr,
-            (PHY_LINK_IS_SPEED_100M(data->state.speed) ? "100" : "10"),
-            PHY_LINK_IS_FULL_DUPLEX(data->state.speed) ? "full" : "half");
+    LOG_INF("PHY %d is %s", cfg->addr, ctx->state.is_up ? "up" : "down");
+    LOG_INF("PHY (%d) Link speed %s Mb, %s duplex\n", cfg->addr,
+            (PHY_LINK_IS_SPEED_100M(ctx->state.speed) ? "100" : "10"),
+            PHY_LINK_IS_FULL_DUPLEX(ctx->state.speed) ? "full" : "half");
 
-done:
+done :
     /* Unlock mutex */
-    k_mutex_unlock(&data->mutex);
+    k_mutex_unlock(&ctx->mutex);
 
     /* Start monitoring */
-    k_work_reschedule(&data->phy_monitor_work,
+    k_work_reschedule(&ctx->phy_monitor_work,
                       K_MSEC(CONFIG_PHY_MONITOR_PERIOD));
 
     return (ret);
@@ -336,62 +336,62 @@ done:
 
 static int phy_mc_lan8742a_link_cb_set(const struct device* dev,
                                        phy_callback_t cb, void* user_data) {
-    struct mc_lan8742a_data* data = dev->data;
+    struct mc_lan8742a_data* ctx = dev->data;
 
-    data->cb = cb;
-    data->cb_data = user_data;
+    ctx->cb = cb;
+    ctx->cb_data = user_data;
 
-    phy_mc_lan8742a_get_link(dev, &data->state);
+    phy_mc_lan8742a_get_link(dev, &ctx->state);
 
-    data->cb(dev, &data->state, data->cb_data);
+    ctx->cb(dev, &ctx->state, ctx->cb_data);
 
     return (0);
 }
 
 static void phy_mc_lan8742a_monitor_work_handler(struct k_work* work) {
     struct k_work_delayable* dwork = k_work_delayable_from_work(work);
-    struct mc_lan8742a_data* data =
+    struct mc_lan8742a_data* ctx =
             CONTAINER_OF(dwork, struct mc_lan8742a_data, phy_monitor_work);
-    const struct device* dev = data->dev;
+    const struct device* dev = ctx->dev;
     struct phy_link_state state;
     int rc;
 
     rc = phy_mc_lan8742a_get_link(dev, &state);
 
     if ((rc == 0) &&
-        (phy_link_state_cmp(&state, &data->state) != 0)) {
-        data->state = state;
-        if (data->cb) {
-            data->cb(dev, &data->state, data->cb_data);
+        (phy_link_state_cmp(&state, &ctx->state) != 0)) {
+        ctx->state = state;
+        if (ctx->cb) {
+            ctx->cb(dev, &ctx->state, ctx->cb_data);
         }
     }
 
     /* TODO change this to GPIO interrupt driven */
-    k_work_reschedule(&data->phy_monitor_work, K_MSEC(CONFIG_PHY_MONITOR_PERIOD));
+    k_work_reschedule(&ctx->phy_monitor_work, K_MSEC(CONFIG_PHY_MONITOR_PERIOD));
 }
 
 static int phy_mc_lan8742a_init(const struct device* dev) {
-    const struct mc_lan8742a_config* config = dev->config;
-    struct mc_lan8742a_data* data = dev->data;
+    const struct mc_lan8742a_config* cfg = dev->config;
+    struct mc_lan8742a_data* ctx = dev->data;
     int ret;
 
-    data->dev = dev;
+    ctx->dev = dev;
 
-    ret = k_mutex_init(&data->mutex);
-    if (ret) {
+    ret = k_mutex_init(&ctx->mutex);
+    if (ret != 0) {
         return (ret);
     }
 
-    mdio_bus_enable(config->mdio_dev);
+    mdio_bus_enable(cfg->mdio_dev);
 
     #if DT_ANY_INST_HAS_PROP_STATUS_OKAY(mc_interrupt_gpio)
-    if (!config->interrupt_gpio.port) {
+    if (cfg->interrupt_gpio.port == NULL) {
         goto skip_int_gpio;
     }
 
     /* Prevent NAND TREE mode */
-    ret = gpio_pin_configure_dt(&config->interrupt_gpio, GPIO_OUTPUT_ACTIVE);
-    if (ret) {
+    ret = gpio_pin_configure_dt(&cfg->interrupt_gpio, GPIO_OUTPUT_ACTIVE);
+    if (ret != 0) {
         return (ret);
     }
 
@@ -399,13 +399,13 @@ skip_int_gpio :
     #endif /* DT_ANY_INST_HAS_PROP_STATUS_OKAY(mc_interrupt_gpio) */
 
     #if DT_ANY_INST_HAS_PROP_STATUS_OKAY(mc_reset_gpio)
-    if (!config->reset_gpio.port) {
+    if (cfg->reset_gpio.port == NULL) {
         goto skip_reset_gpio;
     }
 
     /* Start reset */
-    ret = gpio_pin_configure_dt(&config->reset_gpio, GPIO_OUTPUT_ACTIVE);
-    if (ret) {
+    ret = gpio_pin_configure_dt(&cfg->reset_gpio, GPIO_OUTPUT_INACTIVE);
+    if (ret != 0) {
         return (ret);
     }
 
@@ -413,15 +413,15 @@ skip_int_gpio :
     k_busy_wait(USEC_PER_MSEC * 500);
 
     /* Reset over */
-    ret = gpio_pin_set_dt(&config->reset_gpio, 0);
-    if (ret) {
+    ret = gpio_pin_set_dt(&cfg->reset_gpio, 1);
+    if (ret != 0) {
         return (ret);
     }
 
 skip_reset_gpio :
     #endif /* DT_ANY_INST_HAS_PROP_STATUS_OKAY(mc_reset_gpio) */
 
-    k_work_init_delayable(&data->phy_monitor_work,
+    k_work_init_delayable(&ctx->phy_monitor_work,
                           phy_mc_lan8742a_monitor_work_handler);
 
     return (0);
