bd259e4043bc02218c8166208f79df026ccdb9de
ndrs-pst <50133174+ndrs-pst@users.noreply.github.com>
2025-01-31 14:41:46 +0700

    fs: fatfs: update FAT FS support code for version 0.15a w/patch1

--- modules/fatfs/zephyr_fatfs_config.h
+++ modules/fatfs/zephyr_fatfs_config.h
@@ -4,8 +4,8 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#if FFCONF_DEF != 80286
-//#error "Configuration version mismatch"
+#if FFCONF_DEF != 5380
+#error "Configuration version mismatch"
 #endif
 
 /*
