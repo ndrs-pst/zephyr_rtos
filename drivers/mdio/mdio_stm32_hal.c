f1d48e8004f6d63fbdfb75b9b434b77a6957d0bc
ndrs-pst <50133174+ndrs-pst@users.noreply.github.com>
2025-11-28 02:15:20 +0700

    drivers: mdio: stm32: drop support for CONFIG_ETH_STM32_HAL_API_V1
    Drop support for V1 API and add clause 45 register access.

--- drivers/mdio/mdio_stm32_hal.c
+++ drivers/mdio/mdio_stm32_hal.c
@@ -13,193 +13,178 @@
 #include <zephyr/drivers/clock_control/stm32_clock_control.h>
 #include <zephyr/drivers/pinctrl.h>
 #include <zephyr/drivers/mdio.h>
-#include <zephyr/net/ethernet.h>
-#include <zephyr/net/mdio.h>
+#include <zephyr/net/mii.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(mdio_stm32_hal, CONFIG_MDIO_LOG_LEVEL);
 
 /* #CUSTOM@NDRS */
-#define DEVICE_STM32_GET_ETH_HNDL(dev)         (ETH_HandleTypeDef*)(&(((struct mdio_stm32_dev_data*)(dev)->data)->heth))
+#define DEVICE_STM32_GET_ETH_HNDL(dev) (ETH_HandleTypeDef*)(&(((struct mdio_stm32_dev_data*)(dev)->data)->heth))
 
 #define DT_DRV_COMPAT st_stm32_mdio
 
-#define ADIN1100_REG_VALUE_MASK		GENMASK(15, 0)
-
 struct ETH_HandlePrivTypeDef {
-    ETH_TypeDef* Instance;                  /*!< Register base address       */
+    ETH_TypeDef* Instance; /*!< Register base address       */
 };
 
 struct mdio_stm32_dev_data {
-	struct ETH_HandlePrivTypeDef heth;
-	struct k_sem sem;
+    struct ETH_HandlePrivTypeDef heth;
+    struct k_sem sem;
 };
 
 struct mdio_stm32_dev_config {
-	const struct pinctrl_dev_config *pincfg;
-	struct stm32_pclken pclken;
+    const struct pinctrl_dev_config* pincfg;
+    struct stm32_pclken pclken;
 };
 
-static int mdio_stm32_read(const struct device *dev, uint8_t prtad,
-			   uint8_t regad, uint16_t *data)
-{
-	struct mdio_stm32_dev_data *const dev_data = dev->data;
-	ETH_HandleTypeDef* heth = DEVICE_STM32_GET_ETH_HNDL(dev);
-	HAL_StatusTypeDef ret;
-	uint32_t read;
+static int mdio_stm32_read(const struct device* dev, uint8_t prtad,
+                           uint8_t regad, uint16_t* data) {
+    struct mdio_stm32_dev_data* const dev_data = dev->data;
+    ETH_HandleTypeDef* heth = DEVICE_STM32_GET_ETH_HNDL(dev);
+    HAL_StatusTypeDef hal_sts;
+    uint32_t read;
 
-	k_sem_take(&dev_data->sem, K_FOREVER);
+    k_sem_take(&dev_data->sem, K_FOREVER);
+    hal_sts = HAL_ETH_ReadPHYRegister(heth, prtad, regad, &read);
+    k_sem_give(&dev_data->sem);
 
-#ifdef CONFIG_ETH_STM32_HAL_API_V2
-	ret = HAL_ETH_ReadPHYRegister(heth, prtad, regad, &read);
-#else
-	heth->Init.PhyAddress = prtad;
+    if (hal_sts != HAL_OK) {
+        return (-EIO);
+    }
 
-	ret = HAL_ETH_ReadPHYRegister(heth, regad, &read);
-#endif
+    *data = (read & GENMASK(15, 0));
 
-	k_sem_give(&dev_data->sem);
-
-	if (ret != HAL_OK) {
-		return -EIO;
-	}
-
-	*data = read & ADIN1100_REG_VALUE_MASK;
-
-	return 0;
+    return (0);
 }
 
-static int mdio_stm32_write(const struct device *dev, uint8_t prtad,
-			    uint8_t regad, uint16_t data)
-{
-	struct mdio_stm32_dev_data *const dev_data = dev->data;
-	ETH_HandleTypeDef* heth = DEVICE_STM32_GET_ETH_HNDL(dev);
-	HAL_StatusTypeDef ret;
+static int mdio_stm32_write(const struct device* dev, uint8_t prtad,
+                            uint8_t regad, uint16_t data) {
+    struct mdio_stm32_dev_data* const ctx = dev->data;
+    ETH_HandleTypeDef* heth = DEVICE_STM32_GET_ETH_HNDL(dev);
+    HAL_StatusTypeDef hal_sts;
 
-	k_sem_take(&dev_data->sem, K_FOREVER);
+    k_sem_take(&ctx->sem, K_FOREVER);
+    hal_sts = HAL_ETH_WritePHYRegister(heth, prtad, regad, data);
+    k_sem_give(&ctx->sem);
 
-#ifdef CONFIG_ETH_STM32_HAL_API_V2
-	ret = HAL_ETH_WritePHYRegister(heth, prtad, regad, data);
-#else
-	heth->Init.PhyAddress = prtad;
+    if (hal_sts != HAL_OK) {
+        return (-EIO);
+    }
 
-	ret = HAL_ETH_WritePHYRegister(heth, regad, data);
-#endif
-
-	k_sem_give(&dev_data->sem);
-
-	if (ret != HAL_OK) {
-		return -EIO;
-	}
-
-	return 0;
+    return (0);
 }
 
-#ifdef CONFIG_ETH_STM32_HAL_API_V1
-static void eth_set_mdio_clock_range_for_hal_v1(ETH_HandleTypeDef *heth)
-{
-	uint32_t tmpreg1 = 0U;
+static int mdio_stm32_c45_setup_dev_reg(const struct device* dev, uint8_t prtad, uint16_t devad, uint16_t reg) {
+    int ret;
 
-	/* Get the ETHERNET MACMIIAR value */
-	tmpreg1 = (heth->Instance)->MACMIIAR;
-	/* Clear CSR Clock Range CR[2:0] bits */
-	tmpreg1 &= ETH_MACMIIAR_CR_MASK;
+    ret = mdio_write(dev, prtad, MII_MMD_ACR, devad);
+    if (ret < 0) {
+        return (ret);
+    }
 
-	/* Set CR bits depending on CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC value */
-#ifdef CONFIG_SOC_SERIES_STM32F1X
-	if ((CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC >= 20000000U) &&
-	    (CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC < 35000000U)) {
-		/* CSR Clock Range between 20-35 MHz */
-		tmpreg1 |= (uint32_t)ETH_MACMIIAR_CR_DIV16;
-	} else if ((CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC >= 35000000U) &&
-		   (CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC < 60000000U)) {
-		/* CSR Clock Range between 35-60 MHz */
-		tmpreg1 |= (uint32_t)ETH_MACMIIAR_CR_DIV26;
-	} else {
-		/* CSR Clock Range between 60-72 MHz */
-		tmpreg1 |= (uint32_t)ETH_MACMIIAR_CR_DIV42;
-	}
-#else  /* CONFIG_SOC_SERIES_STM32F2X */
-	if ((CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC >= 20000000U) &&
-	    (CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC < 35000000U)) {
-		/* CSR Clock Range between 20-35 MHz */
-		tmpreg1 |= (uint32_t)ETH_MACMIIAR_CR_Div16;
-	} else if ((CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC >= 35000000U) &&
-		   (CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC < 60000000U)) {
-		/* CSR Clock Range between 35-60 MHz */
-		tmpreg1 |= (uint32_t)ETH_MACMIIAR_CR_Div26;
-	} else if ((CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC >= 60000000U) &&
-		   (CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC < 100000000U)) {
-		/* CSR Clock Range between 60-100 MHz */
-		tmpreg1 |= (uint32_t)ETH_MACMIIAR_CR_Div42;
-	} else {
-		/* CSR Clock Range between 100-120 MHz */
-		tmpreg1 |= (uint32_t)ETH_MACMIIAR_CR_Div62;
-	}
-#endif /* CONFIG_SOC_SERIES_STM32F1X */
+    ret = mdio_write(dev, prtad, MII_MMD_AADR, reg);
+    if (ret < 0) {
+        return (ret);
+    }
 
-	/* Write to ETHERNET MAC MIIAR: Configure the ETHERNET CSR Clock Range */
-	(heth->Instance)->MACMIIAR = (uint32_t)tmpreg1;
+    ret = mdio_write(dev, prtad, MII_MMD_ACR, MII_MMD_ACR_DATA_NO_POS_INC | devad);
+
+    return (ret);
 }
-#endif /* CONFIG_ETH_STM32_HAL_API_V1 */
 
-static int mdio_stm32_init(const struct device *dev)
-{
-	struct mdio_stm32_dev_data *const dev_data = dev->data;
-	const struct mdio_stm32_dev_config *const config = dev->config;
-	ETH_HandleTypeDef* heth = DEVICE_STM32_GET_ETH_HNDL(dev);
-	int ret;
+/**
+ * @brief Read a PHY register using the MDIO bus clause 45
+ * @param[in] dev   MDIO device
+ * @param[in] prtad PHY port address
+ * @param[in] devad PHY device address
+ * @param[in] regad PHY register address
+ * @param[out] data Pointer to the data to be read
+ * @return 0 on success, negative errno code on fail
+ */
+static int mdio_stm32_read_c45(const struct device* dev, uint8_t prtad, uint8_t devad, uint16_t regad, uint16_t* data) {
+    int ret;
 
-	/* enable clock */
-	ret = clock_control_on(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
-			       (clock_control_subsys_t)&config->pclken);
-	if (ret < 0) {
-		LOG_ERR("Failed to enable ethernet clock needed for MDIO (%d)", ret);
-		return ret;
-	}
+    ret = mdio_stm32_c45_setup_dev_reg(dev, prtad, devad, regad);
+    if (ret < 0) {
+        return (ret);
+    }
 
-	ret = pinctrl_apply_state(config->pincfg, PINCTRL_STATE_DEFAULT);
-	if (ret < 0) {
-		return ret;
-	}
+    ret = mdio_read(dev, prtad, MII_MMD_AADR, data);
 
-#ifdef CONFIG_ETH_STM32_HAL_API_V2
-	HAL_ETH_SetMDIOClockRange(heth);
-#else
-	/* The legacy V1 HAL API does not provide a way to set the MDC clock range
-	 * via a separated function call. Implement an equivalent function ourselves
-	 * based on what the V1 HAL performs in HAL_ETH_Init().
-	 */
-	eth_set_mdio_clock_range_for_hal_v1(heth);
-#endif
+    return (ret);
+}
 
-	k_sem_init(&dev_data->sem, 1, 1);
+/**
+ * @brief Write a PHY register using the MDIO bus clause 45
+ * @param[in] dev MDIO device
+ * @param[in] prtad PHY port address
+ * @param[in] devad PHY device address
+ * @param[in] regad PHY register address
+ * @param[in] data  Data to be written
+ * @return 0 on success, negative errno code on fail
+ */
+static int mdio_stm32_write_c45(const struct device* dev, uint8_t prtad, uint8_t devad, uint16_t regad, uint16_t data) {
+    int ret;
 
-	return 0;
+    ret = mdio_stm32_c45_setup_dev_reg(dev, prtad, devad, regad);
+    if (ret < 0) {
+        return (ret);
+    }
+
+    ret = mdio_write(dev, prtad, MII_MMD_AADR, data);
+
+    return (ret);
+}
+
+
+static int mdio_stm32_init(const struct device* dev) {
+    const struct mdio_stm32_dev_config* const cfg = dev->config;
+    struct mdio_stm32_dev_data* const ctx = dev->data;
+    ETH_HandleTypeDef* heth = DEVICE_STM32_GET_ETH_HNDL(dev);
+    int ret;
+
+    ret = clock_control_on(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+                           (clock_control_subsys_t)&cfg->pclken);
+    if (ret < 0) {
+        LOG_ERR("Failed to enable ethernet clock needed for MDIO (%d)", ret);
+        return (ret);
+    }
+
+    ret = pinctrl_apply_state(cfg->pincfg, PINCTRL_STATE_DEFAULT);
+    if (ret < 0) {
+        return (ret);
+    }
+
+    HAL_ETH_SetMDIOClockRange(heth);
+    k_sem_init(&ctx->sem, 1, 1);
+
+    return (0);
 }
 
 static DEVICE_API(mdio, mdio_stm32_api) = {
-	.read = mdio_stm32_read,
-	.write = mdio_stm32_write,
+    .read      = mdio_stm32_read,
+    .write     = mdio_stm32_write,
+    .read_c45  = mdio_stm32_read_c45,
+    .write_c45 = mdio_stm32_write_c45,
 };
 
-#define MDIO_STM32_HAL_DEVICE(inst)                                                                \
-	PINCTRL_DT_INST_DEFINE(inst);                                                              \
-                                                                                                   \
-	static struct mdio_stm32_dev_data mdio_stm32_dev_data_##inst = {                           \
-		.heth = {.Instance = (ETH_TypeDef *)DT_REG_ADDR(DT_INST_PARENT(inst))},            \
-	};                                                                                         \
-	static struct mdio_stm32_dev_config mdio_stm32_dev_config_##inst = {                       \
-		.pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(inst),                                    \
-		.pclken = STM32_CLOCK_INFO_BY_NAME(DT_INST_PARENT(inst), stm_eth),                 \
-	};                                                                                         \
-	DEVICE_DT_INST_DEFINE(inst, mdio_stm32_init, NULL, &mdio_stm32_dev_data_##inst,            \
-			      &mdio_stm32_dev_config_##inst, POST_KERNEL, CONFIG_MDIO_INIT_PRIORITY,   \
-			      &mdio_stm32_api);
+#define MDIO_STM32_HAL_DEVICE(inst)                             \
+    PINCTRL_DT_INST_DEFINE(inst);                               \
+                                                                \
+    static struct mdio_stm32_dev_data mdio_stm32_dev_data_##inst = {            \
+        .heth = {.Instance = (ETH_TypeDef*)DT_REG_ADDR(DT_INST_PARENT(inst))},  \
+    };                                                          \
+    static struct mdio_stm32_dev_config mdio_stm32_dev_config_##inst = {        \
+        .pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(inst),         \
+        .pclken = STM32_CLOCK_INFO_BY_NAME(DT_INST_PARENT(inst), stm_eth),      \
+    };                                                          \
+    DEVICE_DT_INST_DEFINE(inst, mdio_stm32_init, NULL, &mdio_stm32_dev_data_##inst, \
+                          &mdio_stm32_dev_config_##inst, POST_KERNEL, CONFIG_MDIO_INIT_PRIORITY, \
+                          &mdio_stm32_api);
 
 DT_INST_FOREACH_STATUS_OKAY(MDIO_STM32_HAL_DEVICE)
 
-#if (__GTEST == 1U)                         /* #CUSTOM@NDRS */
+#if (__GTEST == 1U) /* #CUSTOM@NDRS */
 #include "mcu_reg_stub.h"
 
 void zephyr_gtest_mdio_stm32(void) {
