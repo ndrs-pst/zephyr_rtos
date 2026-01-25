f8a7d533e6265502d96f89e65f90c31aec6751c6
ndrs-pst <50133174+ndrs-pst@users.noreply.github.com>
2026-01-25 22:11:05 +0700

    drivers: clock_control: infineon: align code with in-house style
    Refactor to comply with in-house style, this will simplify
    future maintenance and change tracking.

--- drivers/clock_control/clock_control_infineon_fixed_factor_clock.c
+++ drivers/clock_control/clock_control_infineon_fixed_factor_clock.c
@@ -26,116 +26,114 @@
 LOG_MODULE_REGISTER(clock_control_ifx_fixed_factor_clock, CONFIG_CLOCK_CONTROL_LOG_LEVEL);
 
 struct fixed_factor_clock_config {
-	uint32_t divider;
-	uint32_t block; /* ifx_cat1_clock_block */
-	uint32_t instance;
-	uint32_t source_path;
+    uint32_t divider;
+    uint32_t block; /* ifx_cat1_clock_block */
+    uint32_t instance;
+    uint32_t source_path;
 };
 
 #if defined(CONFIG_SOC_SERIES_PSE84) || defined(CONFIG_SOC_SERIES_PSC3)
-static int check_legal_max_min(const struct device *dev)
-{
-	const struct fixed_factor_clock_config *const config = dev->config;
+static int check_legal_max_min(const struct device* dev) {
+    const struct fixed_factor_clock_config* const config = dev->config;
 
-#if defined(CONFIG_SOC_SERIES_PSE84)
-	if (config->block == IFX_HF && config->instance == 0) {
-		if (Cy_SysClk_ClkHfGetFrequency(0) > MHZ(200)) {
-			LOG_ERR("clk_hf0 frequency is greater than legal max 200 MHz");
-			return -EINVAL;
-		}
-	}
-#elif defined(CONFIG_SOC_SERIES_PSC3)
-	if (config->block == IFX_HF && config->instance == 0) {
-		if (Cy_SysClk_ClkHfGetFrequency(0) > MHZ(180)) {
-			LOG_ERR("clk_hf0 frequency is greater than legal max 180 MHz");
-			return -EINVAL;
-		}
-	}
-#endif
+    #if defined(CONFIG_SOC_SERIES_PSE84)
+    if (config->block == IFX_HF && config->instance == 0) {
+        if (Cy_SysClk_ClkHfGetFrequency(0) > MHZ(200)) {
+            LOG_ERR("clk_hf0 frequency is greater than legal max 200 MHz");
+            return -EINVAL;
+        }
+    }
+    #elif defined(CONFIG_SOC_SERIES_PSC3)
+    if (config->block == IFX_HF && config->instance == 0) {
+        if (Cy_SysClk_ClkHfGetFrequency(0) > MHZ(180)) {
+            LOG_ERR("clk_hf0 frequency is greater than legal max 180 MHz");
+            return -EINVAL;
+        }
+    }
+    #endif
 
-	return 0;
+    return 0;
 }
 #endif
 
-static int fixed_factor_clk_init(const struct device *dev)
-{
-	const struct fixed_factor_clock_config *const config = dev->config;
-#if defined(CONFIG_SOC_SERIES_PSE84) || defined(CONFIG_SOC_SERIES_PSC3)
-	uint32_t rslt;
-#endif
-	int err;
+static int fixed_factor_clk_init(const struct device* dev) {
+    const struct fixed_factor_clock_config* const config = dev->config;
+    #if defined(CONFIG_SOC_SERIES_PSE84) || defined(CONFIG_SOC_SERIES_PSC3)
+    uint32_t rslt;
+    #endif
+    int err;
 
-	switch (config->block) {
+    switch (config->block) {
+        case IFX_PATHMUX :
+            #if !defined(CONFIG_SOC_FAMILY_INFINEON_PSOC4)
+            Cy_SysClk_ClkPathSetSource(config->instance, config->source_path);
+            #endif
+            break;
 
-	case IFX_PATHMUX:
-#if !defined(CONFIG_SOC_FAMILY_INFINEON_PSOC4)
-		Cy_SysClk_ClkPathSetSource(config->instance, config->source_path);
-#endif
-		break;
+        case IFX_HF :
+            #if defined(CONFIG_SOC_FAMILY_INFINEON_PSOC4)
+            err = Cy_SysClk_ClkHfSetSource(config->source_path);
+            if (err != CY_SYSCLK_SUCCESS) {
+                return -EIO;
+            }
 
-	case IFX_HF:
-#if defined(CONFIG_SOC_FAMILY_INFINEON_PSOC4)
-		err = Cy_SysClk_ClkHfSetSource(config->source_path);
-		if (err != CY_SYSCLK_SUCCESS) {
-			return -EIO;
-		}
+            /* Note : you can use only 4 divider types
+             *
+             * - IFX_CLK_HF_NO_DIVIDE
+             * - IFX_CLK_HF_DIVIDE_BY_2
+             * - IFX_CLK_HF_DIVIDE_BY_4
+             * - IFX_CLK_HF_DIVIDE_BY_8
+             */
+            Cy_SysClk_ClkHfSetDivider(IFX_PSOC4_HFCLK_DIV(config->divider));
+            #else
+            uint32_t source_instance = 0;
 
-		/* Note : you can use only 4 divider types
-		 *
-		 * - IFX_CLK_HF_NO_DIVIDE
-		 * - IFX_CLK_HF_DIVIDE_BY_2
-		 * - IFX_CLK_HF_DIVIDE_BY_4
-		 * - IFX_CLK_HF_DIVIDE_BY_8
-		 */
-		Cy_SysClk_ClkHfSetDivider(IFX_PSOC4_HFCLK_DIV(config->divider));
-#else
-		uint32_t source_instance = 0;
+            err = Cy_SysClk_ClkHfSetSource(config->instance, source_instance);
+            if (err != CY_SYSCLK_SUCCESS) {
+                return -EIO;
+            }
 
-		err = Cy_SysClk_ClkHfSetSource(config->instance, source_instance);
-		if (err != CY_SYSCLK_SUCCESS) {
-			return -EIO;
-		}
+            err = Cy_SysClk_ClkHfSetDivider(config->instance, config->divider);
+            if (err != CY_SYSCLK_SUCCESS) {
+                return -EIO;
+            }
 
-		err = Cy_SysClk_ClkHfSetDivider(config->instance, config->divider);
-		if (err != CY_SYSCLK_SUCCESS) {
-			return -EIO;
-		}
+            err = Cy_SysClk_ClkHfEnable(config->instance);
+            if (err != CY_SYSCLK_SUCCESS) {
+                return -EIO;
+            }
+            #endif
+            break;
 
-		err = Cy_SysClk_ClkHfEnable(config->instance);
-		if (err != CY_SYSCLK_SUCCESS) {
-			return -EIO;
-		}
-#endif
-		break;
+        case IFX_PUMP :
+            #if defined(CONFIG_SOC_FAMILY_INFINEON_PSOC4)
+            err = Cy_SysClk_ClkPumpSetSource(config->source_path);
+            if (err != CY_SYSCLK_SUCCESS) {
+                return -EIO;
+            }
+            #endif
+            break;
 
-	case IFX_PUMP:
-#if defined(CONFIG_SOC_FAMILY_INFINEON_PSOC4)
-		err = Cy_SysClk_ClkPumpSetSource(config->source_path);
-		if (err != CY_SYSCLK_SUCCESS) {
-			return -EIO;
-		}
-#endif
-		break;
+        default :
+            return -EINVAL;
+    }
 
-	default:
-		return -EINVAL;
-	}
+    #if defined(CONFIG_SOC_SERIES_PSE84) || defined(CONFIG_SOC_SERIES_PSC3)
+    rslt = check_legal_max_min(dev);
+    #endif
 
-#if defined(CONFIG_SOC_SERIES_PSE84) || defined(CONFIG_SOC_SERIES_PSC3)
-	rslt = check_legal_max_min(dev);
-#endif
-	return 0;
+    return 0;
 }
 
-#define FIXED_CLK_INIT(n)                                                                          \
-	static const struct fixed_factor_clock_config fixed_factor_clock_config_##n = {            \
-		.divider = DT_INST_PROP_OR(n, clock_div, 1u),                                      \
-		.block = DT_INST_PROP(n, system_clock),                                            \
-		.instance = DT_INST_PROP(n, instance),                                             \
-		.source_path = DT_INST_PROP_OR(n, source_path, 1u),                                \
-	};                                                                                         \
-	DEVICE_DT_INST_DEFINE(n, fixed_factor_clk_init, NULL, NULL,                                \
-			      &fixed_factor_clock_config_##n, PRE_KERNEL_1,                        \
-			      CONFIG_CLOCK_CONTROL_IFX_FIXED_FACTOR_CLOCK_INIT_PRIORITY, NULL);
+#define FIXED_CLK_INIT(n)                   \
+    static struct fixed_factor_clock_config DT_CONST fixed_factor_clock_config_##n = { \
+        .divider     = DT_INST_PROP_OR(n, clock_div, 1u),       \
+        .block       = DT_INST_PROP(n, system_clock),           \
+        .instance    = DT_INST_PROP(n, instance),               \
+        .source_path = DT_INST_PROP_OR(n, source_path, 1u),     \
+    };                                                          \
+    DEVICE_DT_INST_DEFINE(n, fixed_factor_clk_init, NULL, NULL, \
+                          &fixed_factor_clock_config_##n, PRE_KERNEL_1, \
+                          CONFIG_CLOCK_CONTROL_IFX_FIXED_FACTOR_CLOCK_INIT_PRIORITY, NULL);
 
 DT_INST_FOREACH_STATUS_OKAY(FIXED_CLK_INIT)
--- drivers/clock_control/clock_control_infineon_peri_clock.c
+++ drivers/clock_control/clock_control_infineon_peri_clock.c
@@ -23,10 +23,10 @@
 #include <cy_systick.h>
 
 struct ifx_peri_clock_data {
-	struct ifx_cat1_resource_inst hw_resource;
-	struct ifx_cat1_clock clock;
-	uint16_t divider;
-	CySCB_Type *reg_addr;
+    struct ifx_cat1_resource_inst hw_resource;
+    struct ifx_cat1_clock clock;
+    uint16_t divider;
+    CySCB_Type* reg_addr;
 };
 
 #if defined(CY_IP_MXPERI) || defined(CY_IP_M0S8PERI)
@@ -57,146 +57,156 @@ struct ifx_peri_clock_data {
 
 #define CLK_FRAC_DIV_MODE 0x02
 
-en_clk_dst_t ifx_cat1_scb_get_clock_index(uint32_t block_num)
-{
-	en_clk_dst_t clk;
-/* PSOC6A256K does not have SCB 3 */
-#if defined(CY_DEVICE_PSOC6A256K)
-	if (block_num < 3) {
-		clk = (en_clk_dst_t)((uint32_t)IFX_SCB0_PCLK_CLOCK + block_num);
-	} else {
-		clk = (en_clk_dst_t)((uint32_t)IFX_SCB0_PCLK_CLOCK + block_num - 1);
-	}
-#elif defined(CONFIG_SOC_FAMILY_INFINEON_EDGE)
-	if (block_num == 0) {
-		clk = (en_clk_dst_t)((uint32_t)IFX_SCB0_PCLK_CLOCK);
-	} else if (block_num == 1) {
-		clk = (en_clk_dst_t)((uint32_t)IFX_SCB1_PCLK_CLOCK);
-	} else {
-		clk = (en_clk_dst_t)((uint32_t)IFX_SCB0_PCLK_CLOCK + block_num - 1);
-	}
-#else
-	clk = (en_clk_dst_t)((uint32_t)IFX_SCB0_PCLK_CLOCK + block_num);
-#endif
+en_clk_dst_t ifx_cat1_scb_get_clock_index(uint32_t block_num) {
+    en_clk_dst_t clk;
 
-	return clk;
+    /* PSOC6A256K does not have SCB 3 */
+    #if defined(CY_DEVICE_PSOC6A256K)
+    if (block_num < 3) {
+        clk = (en_clk_dst_t)((uint32_t)IFX_SCB0_PCLK_CLOCK + block_num);
+    }
+    else {
+        clk = (en_clk_dst_t)((uint32_t)IFX_SCB0_PCLK_CLOCK + block_num - 1);
+    }
+    #elif defined(CONFIG_SOC_FAMILY_INFINEON_EDGE)
+    if (block_num == 0) {
+        clk = (en_clk_dst_t)((uint32_t)IFX_SCB0_PCLK_CLOCK);
+    }
+    else if (block_num == 1) {
+        clk = (en_clk_dst_t)((uint32_t)IFX_SCB1_PCLK_CLOCK);
+    }
+    else {
+        clk = (en_clk_dst_t)((uint32_t)IFX_SCB0_PCLK_CLOCK + block_num - 1);
+    }
+    #else
+    clk = (en_clk_dst_t)((uint32_t)IFX_SCB0_PCLK_CLOCK + block_num);
+    #endif
+
+    return clk;
 }
 
-en_clk_dst_t ifx_cat1_tcpwm_get_clock_index(uint32_t block_num, uint32_t channel)
-{
-	en_clk_dst_t clk = -EINVAL;
+en_clk_dst_t ifx_cat1_tcpwm_get_clock_index(uint32_t block_num, uint32_t channel) {
+    en_clk_dst_t clk = -EINVAL;
 
-	if (block_num == 0) {
-		/* block_num 0 is 32-bit tcpwm instances */
-		clk = (en_clk_dst_t)((uint32_t)IFX_TCPWM0_PCLK_CLOCK0 + channel);
-#if (CY_IP_MXTCPWM_INSTANCES > 1) || (CY_IP_M0S8TCPWM_INSTANCES > 1) || (CY_IP_MXSPERI)
-	} else if (block_num == 1) {
-		/* block_num 1 is 16-bit tcwpm instances */
-		clk = (en_clk_dst_t)((uint32_t)IFX_TCPWM1_PCLK_CLOCK0 + channel);
-#endif
-	} else {
-		/* Current support does not account for block_num other than 0 or 1 */
-		__ASSERT(block_num == 0 || block_num == 1,
-			 "Invalid block_num used for tcpwm clock index.");
-#if (CY_IP_MXTCPWM_INSTANCES == 1) || (CY_IP_M0S8TCPWM_INSTANCES == 1) || (CY_IP_MXSPERI)
-		__ASSERT(block_num == 0, "Invalid block_num used for tcpwm clock index.");
-#endif
-	}
+    if (block_num == 0) {
+        /* block_num 0 is 32-bit tcpwm instances */
+        clk = (en_clk_dst_t)((uint32_t)IFX_TCPWM0_PCLK_CLOCK0 + channel);
+    #if (CY_IP_MXTCPWM_INSTANCES > 1) || (CY_IP_M0S8TCPWM_INSTANCES > 1) || (CY_IP_MXSPERI)
+    }
+    else if (block_num == 1) {
+        /* block_num 1 is 16-bit tcwpm instances */
+        clk = (en_clk_dst_t)((uint32_t)IFX_TCPWM1_PCLK_CLOCK0 + channel);
+    #endif
+    }
+    else {
+        /* Current support does not account for block_num other than 0 or 1 */
+        __ASSERT((block_num == 0) || (block_num == 1),
+                 "Invalid block_num used for tcpwm clock index.");
+        #if (CY_IP_MXTCPWM_INSTANCES == 1) || (CY_IP_M0S8TCPWM_INSTANCES == 1) || (CY_IP_MXSPERI)
+        __ASSERT(block_num == 0, "Invalid block_num used for tcpwm clock index.");
+        #endif
+    }
 
-	return clk;
+    return clk;
 }
 
-en_clk_dst_t ifx_cat1_sdhc_get_clock_index(uint32_t block_num, uint32_t channel)
-{
-	en_clk_dst_t clk = -EINVAL;
+en_clk_dst_t ifx_cat1_sdhc_get_clock_index(uint32_t block_num, uint32_t channel) {
+    en_clk_dst_t clk = -EINVAL;
 
-#if defined(CY_IP_MXSDHC)
-	if (block_num == 0) {
-		clk = (en_clk_dst_t)((uint32_t)IFX_SDHC0_PCLK_CLOCK);
-	} else {
-		clk = (en_clk_dst_t)((uint32_t)IFX_SDHC1_PCLK_CLOCK);
-	}
-#endif
+    #if defined(CY_IP_MXSDHC)
+    if (block_num == 0) {
+        clk = (en_clk_dst_t)((uint32_t)IFX_SDHC0_PCLK_CLOCK);
+    }
+    else {
+        clk = (en_clk_dst_t)((uint32_t)IFX_SDHC1_PCLK_CLOCK);
+    }
+    #endif
 
-	return clk;
+    return clk;
 }
 
-static int ifx_cat1_peri_clock_init(const struct device *dev)
-{
-	struct ifx_peri_clock_data *const data = dev->data;
-	int err;
+static int ifx_cat1_peri_clock_init(const struct device* dev) {
+    struct ifx_peri_clock_data* const data = dev->data;
+    int err;
 
-	if (data->hw_resource.type == IFX_RSC_SCB) {
-		en_clk_dst_t clk_idx = ifx_cat1_scb_get_clock_index(data->hw_resource.block_num);
+    if (data->hw_resource.type == IFX_RSC_SCB) {
+        en_clk_dst_t clk_idx = ifx_cat1_scb_get_clock_index(data->hw_resource.block_num);
 
-		if ((data->clock.block & CLK_FRAC_DIV_MODE) == 0) {
-			err = ifx_cat1_utils_peri_pclk_set_divider(clk_idx, &data->clock,
-								   data->divider - 1);
-		} else {
-			err = ifx_cat1_utils_peri_pclk_set_frac_divider(clk_idx, &(data->clock),
-									data->divider - 1, 0);
-		}
+        if ((data->clock.block & CLK_FRAC_DIV_MODE) == 0) {
+            err = ifx_cat1_utils_peri_pclk_set_divider(clk_idx, &data->clock,
+                                                       data->divider - 1);
+        }
+        else {
+            err = ifx_cat1_utils_peri_pclk_set_frac_divider(clk_idx, &(data->clock),
+                                                            data->divider - 1, 0);
+        }
 
-		if (err != CY_SYSCLK_SUCCESS) {
-			return -EIO;
-		}
+        if (err != CY_SYSCLK_SUCCESS) {
+            return -EIO;
+        }
 
-		err = ifx_cat1_utils_peri_pclk_assign_divider(clk_idx, &data->clock);
-		if (err != CY_SYSCLK_SUCCESS) {
-			return -EIO;
-		}
+        err = ifx_cat1_utils_peri_pclk_assign_divider(clk_idx, &data->clock);
+        if (err != CY_SYSCLK_SUCCESS) {
+            return -EIO;
+        }
 
-		err = ifx_cat1_utils_peri_pclk_enable_divider(clk_idx, &data->clock);
-		if (err != CY_SYSCLK_SUCCESS) {
-			return -EIO;
-		}
-	} else if (data->hw_resource.type == IFX_RSC_TCPWM) {
-		en_clk_dst_t clk_idx = ifx_cat1_tcpwm_get_clock_index(
-			data->hw_resource.block_num, data->hw_resource.channel_num);
+        err = ifx_cat1_utils_peri_pclk_enable_divider(clk_idx, &data->clock);
+        if (err != CY_SYSCLK_SUCCESS) {
+            return -EIO;
+        }
+    }
+    else if (data->hw_resource.type == IFX_RSC_TCPWM) {
+        en_clk_dst_t clk_idx = ifx_cat1_tcpwm_get_clock_index(
+            data->hw_resource.block_num, data->hw_resource.channel_num);
 
-		ifx_cat1_utils_peri_pclk_set_divider(clk_idx, &data->clock, data->divider - 1);
-		ifx_cat1_utils_peri_pclk_assign_divider(clk_idx, &data->clock);
-		ifx_cat1_utils_peri_pclk_enable_divider(clk_idx, &data->clock);
-	} else if (data->hw_resource.type == IFX_RSC_SDHC) {
-		en_clk_dst_t clk_idx = ifx_cat1_sdhc_get_clock_index(data->hw_resource.block_num,
-								     data->hw_resource.channel_num);
+        ifx_cat1_utils_peri_pclk_set_divider(clk_idx, &data->clock, data->divider - 1);
+        ifx_cat1_utils_peri_pclk_assign_divider(clk_idx, &data->clock);
+        ifx_cat1_utils_peri_pclk_enable_divider(clk_idx, &data->clock);
+    }
+    else if (data->hw_resource.type == IFX_RSC_SDHC) {
+        en_clk_dst_t clk_idx = ifx_cat1_sdhc_get_clock_index(data->hw_resource.block_num,
+                                                             data->hw_resource.channel_num);
 
-		ifx_cat1_utils_peri_pclk_set_divider(clk_idx, &data->clock, data->divider - 1);
-		ifx_cat1_utils_peri_pclk_assign_divider(clk_idx, &data->clock);
-		ifx_cat1_utils_peri_pclk_enable_divider(clk_idx, &data->clock);
-	} else {
-		return -EINVAL;
-	}
+        ifx_cat1_utils_peri_pclk_set_divider(clk_idx, &data->clock, data->divider - 1);
+        ifx_cat1_utils_peri_pclk_assign_divider(clk_idx, &data->clock);
+        ifx_cat1_utils_peri_pclk_enable_divider(clk_idx, &data->clock);
+    }
+    else {
+        return -EINVAL;
+    }
 
-	return 0;
+    return 0;
 }
 
 #if defined(CONFIG_SOC_FAMILY_INFINEON_EDGE)
-#define PERI_CLOCK_INIT(n)                                                                         \
-	.clock = {                                                                                 \
-		.block = IFX_CAT1_PERIPHERAL_GROUP_ADJUST(DT_INST_PROP_BY_IDX(n, peri_group, 0),   \
-							  DT_INST_PROP_BY_IDX(n, peri_group, 1),   \
-							  DT_INST_PROP(n, div_type)),              \
-		.channel = DT_INST_PROP(n, channel),                                               \
-	},
+#define PERI_CLOCK_INIT(n)                                      \
+    .clock = {                                                  \
+        .block   = IFX_CAT1_PERIPHERAL_GROUP_ADJUST(DT_INST_PROP_BY_IDX(n, peri_group, 0), \
+                                                    DT_INST_PROP_BY_IDX(n, peri_group, 1), \
+                                                    DT_INST_PROP(n, div_type)), \
+        .channel = DT_INST_PROP(n, channel),                    \
+    },
 #else
-#define PERI_CLOCK_INIT(n)                                                                         \
-	.clock = {                                                                                 \
-		.block = IFX_CAT1_PERIPHERAL_GROUP_ADJUST(DT_INST_PROP_BY_IDX(n, peri_group, 1),   \
-							  DT_INST_PROP(n, div_type)),              \
-		.channel = DT_INST_PROP(n, channel),                                               \
-	},
+#define PERI_CLOCK_INIT(n)                                      \
+    .clock = {                                                  \
+        .block   = IFX_CAT1_PERIPHERAL_GROUP_ADJUST(DT_INST_PROP_BY_IDX(n, peri_group, 1), \
+                                                    DT_INST_PROP(n, div_type)), \
+        .channel = DT_INST_PROP(n, channel),                    \
+    },
 #endif
 
-#define INFINEON_CAT1_PERI_CLOCK_INIT(n)                                                           \
-	static struct ifx_peri_clock_data ifx_cat1_peri_clock##n##_data = {                        \
-		.divider = DT_INST_PROP(n, clock_div),                                             \
-		.hw_resource = {.type = DT_INST_PROP(n, resource_type),                            \
-				.block_num = DT_INST_PROP(n, resource_instance),                   \
-				.channel_num = DT_INST_PROP_OR(n, resource_channel, 0)},           \
-		PERI_CLOCK_INIT(n)};                                                               \
-                                                                                                   \
-	DEVICE_DT_INST_DEFINE(n, &ifx_cat1_peri_clock_init, NULL, &ifx_cat1_peri_clock##n##_data,  \
-			      NULL, PRE_KERNEL_1, CONFIG_CLOCK_CONTROL_INIT_PRIORITY, NULL);
+#define INFINEON_CAT1_PERI_CLOCK_INIT(n)                        \
+    static struct ifx_peri_clock_data ifx_cat1_peri_clock##n##_data = { \
+        .divider = DT_INST_PROP(n, clock_div),                  \
+        .hw_resource = {                                        \
+            .type        = DT_INST_PROP(n, resource_type),      \
+            .block_num   = DT_INST_PROP(n, resource_instance),  \
+            .channel_num = DT_INST_PROP_OR(n, resource_channel, 0) \
+        },                                                      \
+        PERI_CLOCK_INIT(n)                                      \
+    };                                                          \
+                                                                \
+    DEVICE_DT_INST_DEFINE(n, ifx_cat1_peri_clock_init, NULL, &ifx_cat1_peri_clock##n##_data, \
+                          NULL, PRE_KERNEL_1, CONFIG_CLOCK_CONTROL_INIT_PRIORITY, NULL);
 
 DT_INST_FOREACH_STATUS_OKAY(INFINEON_CAT1_PERI_CLOCK_INIT)
